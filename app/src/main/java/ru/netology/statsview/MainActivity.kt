fe63159d2e65c096c488729f6ebaacf6fde25c00
Evgeny Zemtsov <evan77@bk.ru>
2024-03-15 23:29:37 +0300

    animation part2

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -1,11 +1,17 @@
 package ru.netology.statsview
 
+import android.animation.Animator
 import android.animation.Animator.AnimatorListener
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.animation.Animation
 import ru.netology.statsview.ui.StatsView
 import android.view.animation.AnimationUtils
+import android.view.animation.BounceInterpolator
+import android.view.animation.LinearInterpolator
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -15,30 +21,25 @@ class MainActivity : AppCompatActivity() {
 
         val view = findViewById<StatsView>(R.id.statsView)
         view.data = listOf(
-            42F,
-            10F,
-            8F,
-            15F,
-            35F,
+            50F,
+            60F,
+            70F,
+            60F,
+            40F,
         )
 
         val label = findViewById<TextView>(R.id.label)
-        view.startAnimation(
-            AnimationUtils.loadAnimation(this, R.anim.animation).apply {
-                setAnimationListener(object : Animation.AnimationListener{
-                    override fun onAnimationStart(animation: Animation?) {
-                        label.text = "started"
-                    }
 
-                    override fun onAnimationEnd(animation: Animation?) {
-                        label.text = "ended"
-                    }
+        label.text = view.dataStr
+//        val rotation = PropertyValuesHolder.ofFloat(View.ROTATION, 0F, 360F)
+//        val alpha = PropertyValuesHolder.ofFloat(View.ALPHA, 0F, 1F)
+//
+//        ObjectAnimator.ofPropertyValuesHolder(view, rotation, alpha)
+//            .apply {
+//                startDelay = 0
+//                duration = 1500
+//                interpolator = LinearInterpolator()
+//            }.start()
 
-                    override fun onAnimationRepeat(animation: Animation?) {
-                        label.text = "repeat"
-                    }
-                })
-            }
-        )
     }
 }
--- app/src/main/java/ru/netology/statsview/ui/StatsView.kt
+++ app/src/main/java/ru/netology/statsview/ui/StatsView.kt
@@ -1,5 +1,6 @@
 package ru.netology.statsview.ui
 
+import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
@@ -8,6 +9,7 @@ import android.graphics.PointF
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.core.content.withStyledAttributes
 import ru.netology.statsview.R
 import ru.netology.statsview.utils.AndroidUtils
@@ -29,6 +31,21 @@ class StatsView @JvmOverloads constructor(
     private var radius = 0F
     private var center = PointF()
     private var oval = RectF()
+    private var valueAnimator: ValueAnimator? = null
+    private var progress = 0F
+    private var modeAnimation: Int = 0
+    private var currentIndexAngel = 0
+    private var sumAngel = 0F
+
+    var data: List<Float> = emptyList()
+        set(value) {
+            val newValue = value.map {
+                it / value.sum()
+            }
+            field = newValue
+            update()
+            //invalidate()
+        }
 
     init {
         context.withStyledAttributes(
@@ -37,24 +54,18 @@ class StatsView @JvmOverloads constructor(
         ) {
             textSize = getDimension(R.styleable.StatsView_textSize, textSize)
             lineWide = getDimension(R.styleable.StatsView_lineWidth, lineWide.toFloat()).toInt()
+            modeAnimation = getInteger(R.styleable.StatsView_modeAnimation, 0)
             colors = listOf(
                 getColor(R.styleable.StatsView_colors1, randomColor()),
                 getColor(R.styleable.StatsView_colors2, randomColor()),
                 getColor(R.styleable.StatsView_colors3, randomColor()),
                 getColor(R.styleable.StatsView_colors4, randomColor()),
             )
+
         }
     }
 
-    var data: List<Float> = emptyList()
-        set(value) {
-            val newValue = value.map {
-                it / value.sum()
-            }
-            field = newValue
-            invalidate()
-        }
-
+    var dataStr: String = ""
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
         .apply {
             strokeWidth = lineWide.toFloat()
@@ -88,40 +99,226 @@ class StatsView @JvmOverloads constructor(
             return
         }
 
-
         var startEngel = -90F
-        var countData = data.count()
-        var colorCircle: Int = 0
+        val countData = data.count()
+        var colorCircle = 0
 
-        data.forEachIndexed() { index, datum ->
-            val angel = datum * 360
-            if (index == countData - 1) {
-                paint.color = 0xFFffffff.toInt()
-                val endAngel = (-360 + angel) * PI / 180
-                val endX = center.x - radius * sin(endAngel).toFloat()
-                val endY = center.y - radius * cos(endAngel).toFloat()
-                canvas.drawPoint(endX, endY, paint)
+        when (modeAnimation) {
+            1 -> {
+                data.forEachIndexed { index, datum ->
+                    val angel = datum * 360
+                    if (index == countData - 1) {
+                        paint.color = 0xFFffffff.toInt()
+                        val endAngel = (-360 + angel) * PI / 180
+                        val endX = center.x - radius * sin(endAngel).toFloat()
+                        val endY = center.y - radius * cos(endAngel).toFloat()
+                        canvas.drawPoint(endX, endY, paint)
 
-                paint.color = 0x55999999.toInt()
+                        paint.color = 0x55999999
 
-            } else {
-                paint.color = colors.getOrElse(index) { randomColor() }
-                if (index == 0) {
-                    colorCircle = paint.color
+                    } else {
+                        paint.color = colors.getOrElse(index) { randomColor() }
+                        if (index == 0) {
+                            colorCircle = paint.color
+                        }
+                    }
+                    canvas.drawArc(oval, startEngel, angel * progress, false, paint)
+                    startEngel += angel
+                }
+                paint.color = colorCircle
+                canvas.drawPoint(center.x, center.y - radius, paint)
+            }
+
+            2 -> {
+
+                var angel = 360 * progress
+                startEngel = -90F
+
+                if (currentIndexAngel == 0) sumAngel = data[0] * 360
+
+                if (angel > sumAngel) {
+                    currentIndexAngel++
+                    sumAngel += data[currentIndexAngel] * 360
+                }
+                when (currentIndexAngel) {
+                    0 -> {
+                        paint.color = colors.getOrElse(currentIndexAngel) { randomColor() }
+                        canvas.drawArc(oval, startEngel, angel, false, paint)
+                    }
+
+                    1 -> {
+
+                        angel -= data[0] * 360
+                        paint.color = colors.getOrElse(currentIndexAngel - 1) { randomColor() }
+                        canvas.drawArc(oval, startEngel, data[0] * 360, false, paint)
+
+                        if (currentIndexAngel == countData - 1) paint.color = 0x55999999
+                        else paint.color = colors.getOrElse(currentIndexAngel) { randomColor() }
+                        canvas.drawArc(
+                            oval,
+                            startEngel + data[0] * 360,
+                            angel * progress,
+                            false,
+                            paint
+                        )
+                    }
+
+                    2 -> {
+                        angel -= (data[0] + data[1]) * 360
+                        paint.color = colors.getOrElse(currentIndexAngel - 2) { randomColor() }
+                        canvas.drawArc(oval, startEngel, data[0] * 360, false, paint)
+
+                        paint.color = colors.getOrElse(currentIndexAngel - 1) { randomColor() }
+                        canvas.drawArc(
+                            oval,
+                            startEngel + data[0] * 360,
+                            data[1] * 360,
+                            false,
+                            paint
+                        )
+
+                        if (currentIndexAngel == countData - 1) paint.color = 0x55999999
+                        else paint.color = colors.getOrElse(currentIndexAngel) { randomColor() }
+                        canvas.drawArc(
+                            oval,
+                            startEngel + (data[0] + data[1]) * 360,
+                            angel * progress,
+                            false,
+                            paint
+                        )
+                    }
+
+                    3 -> {
+                        angel -= (data[0] + data[1] + data[2]) * 360
+                        paint.color = colors.getOrElse(currentIndexAngel - 3) { randomColor() }
+                        canvas.drawArc(oval, startEngel, data[0] * 360, false, paint)
+
+                        paint.color = colors.getOrElse(currentIndexAngel - 2) { randomColor() }
+                        canvas.drawArc(
+                            oval,
+                            startEngel + data[0] * 360,
+                            data[1] * 360,
+                            false,
+                            paint
+                        )
+
+                        paint.color = colors.getOrElse(currentIndexAngel - 1) { randomColor() }
+                        canvas.drawArc(
+                            oval,
+                            startEngel + (data[0] + data[1]) * 360,
+                            data[2] * 360,
+                            false,
+                            paint
+                        )
+
+                        if (currentIndexAngel == countData - 1) paint.color = 0x55999999
+                        else paint.color = colors.getOrElse(currentIndexAngel) { randomColor() }
+                        canvas.drawArc(
+                            oval,
+                            startEngel + (data[0] + data[1] + data[2]) * 360,
+                            angel * progress,
+                            false,
+                            paint
+                        )
+                    }
+
+                    4 -> {
+                        angel -= (data[0] + data[1] + data[2] + data[3]) * 360
+                        paint.color = colors.getOrElse(currentIndexAngel - 4) { randomColor() }
+                        canvas.drawArc(oval, startEngel, data[0] * 360, false, paint)
+
+                        paint.color = colors.getOrElse(currentIndexAngel - 3) { randomColor() }
+                        canvas.drawArc(
+                            oval,
+                            startEngel + data[0] * 360,
+                            data[1] * 360,
+                            false,
+                            paint
+                        )
+
+                        paint.color = colors.getOrElse(currentIndexAngel - 2) { randomColor() }
+                        canvas.drawArc(
+                            oval,
+                            startEngel + (data[0] + data[1]) * 360,
+                            data[2] * 360,
+                            false,
+                            paint
+                        )
+
+                        paint.color = colors.getOrElse(currentIndexAngel - 1) { randomColor() }
+                        canvas.drawArc(
+                            oval,
+                            startEngel + (data[0] + data[1] + data[2]) * 360,
+                            data[3] * 360,
+                            false,
+                            paint
+                        )
+
+                        paint.color = 0x55999999
+                        canvas.drawArc(
+                            oval,
+                            startEngel + (data[0] + data[1] + data[2] + data[3]) * 360,
+                            angel * progress,
+                            false,
+                            paint
+                        )
+
+                    }
+
                 }
             }
 
-            canvas.drawArc(oval, startEngel, angel, false, paint)
-            startEngel += angel
+            3 -> {
+                startEngel = -90F + data[0] * 360 / 2
+                data.forEachIndexed { index, datum ->
+                    val angel = datum * 360
+                    paint.color = colors.getOrElse(index) { randomColor() }
+
+                    if (index == countData - 1) {
+                        paint.color = 0xff999999.toInt()
+                    }
+                    canvas.drawArc(oval, startEngel, angel * progress / 2, false, paint)
+                    canvas.drawArc(oval, startEngel, -angel * progress / 2, false, paint)
+
+                    if (index != countData - 1) startEngel += angel / 2 + data[index + 1] * 360 / 2
+                }
+            }
         }
-        paint.color = colorCircle
-        canvas.drawPoint(center.x, center.y - radius, paint)
-        canvas.drawText(
-            "%.2f%%".format((data.sum() - data.get(countData - 1)) * 100),
-            center.x,
-            center.y + textPaint.textSize / 4,
-            textPaint
-        )
+
+
+//        canvas.drawText(
+//            "%.2f%%".format((data.sum() - data.get(countData - 1)) * 100),
+//            center.x,
+//            center.y + textPaint.textSize / 4,
+//            textPaint
+//        )
+
+    }
+
+    @SuppressLint("Recycle")
+    private fun update() {
+        dataStr = "%.2f%%".format((data.sum() - data[data.count() - 1]) * 100)
+        valueAnimator?.let {
+            it.removeAllListeners()
+            it.cancel()
+        }
+        progress = 0F
+        valueAnimator = ValueAnimator.ofFloat(0F, 1F)
+            .apply {
+                addUpdateListener { anim ->
+                    progress = anim.animatedValue as Float
+                    if (modeAnimation == 1) rotation = 360 * progress
+                    invalidate()
+                }
+                duration = 1500
+                //startDelay = delay.toLong()
+
+                interpolator = LinearInterpolator()
+            }.also {
+                it.start()
+            }
+
+
     }
 
     private fun randomColor() = kotlin.random.Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
